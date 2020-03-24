85920e46d437ca63c01e6af1781cd7ba45910b0d
mattfan00 <kungfumo@gmail.com>
2020-03-24 15:28:41 -0400

    added hover items

--- src/Todos/TodoItem.js
+++ src/Todos/TodoItem.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function TodoItem(props) {
-  const isCompleted = props.completed 
+class TodoItem extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hover: false
+    }
+
+    this.toggleHover = this.toggleHover.bind(this)
+  }
+
+  toggleHover() {
+    this.setState({hover: !this.state.hover})
+  }
+  
+  render() {
+    const isCompleted = this.props.completed 
+    const hover = this.state.hover
+
+    return (
+      <div className="todo-item" onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
+        <span onClick={this.props.toggleEditing} style={{textDecoration: isCompleted ? 'line-through' : 'none'}}>
+          {this.props.name}
+        </span>
+        &nbsp; 
+        {this.props.due ? this.props.due.toLocaleDateString() : ''}
+        &nbsp;
+        {hover ? <HoverItems /> : ''}
+      </div>
+    )
+  }
+}
+
+function HoverItems(props) {
   return (
-    <div className="todo-item">
-      <span onClick={props.toggleEditing} style={{textDecoration: isCompleted ? 'line-through' : 'none'}}>
-        {props.name}
-      </span>
-      &nbsp; 
-      {props.due ? props.due.toLocaleDateString() : ''}
-      &nbsp;
+    <span>
       <span onClick={props.removeTodo}>
-        X
+          X
       </span>
-    </div>
+      &nbsp;
+      <i class="far fa-calendar-plus"></i>
+    </span>
+   
   )
 }
 
