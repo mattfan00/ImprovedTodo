606fd808c6a82326863afed1c94cc23659e2f45d
mattfan00 <kungfumo@gmail.com>
2020-03-25 15:13:18 -0400

    added some styling to create calendar and changed layout of classes from horizontal to vertical

---
+++ src/Todos/Calendar.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react"
+import '../TodoCalendar.css'
+
+class Calendar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+    }
+
+    
+  }
+
+  render() {
+    return (
+      <span className="add-todo-calendar">
+        hello
+      </span>
+    )
+  }
+}
+
+export default Calendar
--- src/Todos/TodoItem.js
+++ src/Todos/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Calendar from './Calendar'
 
 class TodoItem extends Component {
   constructor(props) {
@@ -6,44 +7,19 @@ class TodoItem extends Component {
 
     this.state = {
       hover: false,
-    }
-
-    this.toggleHover = this.toggleHover.bind(this)
-  }
-
-  toggleHover() {
-    this.setState({hover: !this.state.hover})
-  }
-  
-  render() {
-    const isCompleted = this.props.completed 
-    const hover = this.state.hover
-
-    return (
-      <div className="todo-item" onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
-        <span onClick={this.props.toggleEditing} style={{textDecoration: isCompleted ? 'line-through' : 'none'}}>
-          {this.props.name}
-        </span>
-        &nbsp; 
-        {this.props.due ? this.props.due.toLocaleDateString() : ''}
-        &nbsp;
-        {hover ? <HoverItems /> : ''}
-      </div>
-    )
-  }
-}
-
-
-class HoverItems extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
       showMenu: false
     }
 
     this.showMenu = this.showMenu.bind(this)
     this.closeMenu = this.closeMenu.bind(this)
+  }
+
+  toggleHover(enter) {
+    if (enter || this.state.showMenu) {
+      this.setState({hover: true})
+    } else {
+      this.setState({hover: false})
+    }
   }
 
   showMenu(e) {
@@ -55,44 +31,63 @@ class HoverItems extends Component {
   }
 
   closeMenu(e) {
-    
     if (!this.dropdownMenu.contains(e.target)) {
-      
       this.setState({ showMenu: false }, () => {
         document.removeEventListener('click', this.closeMenu);
       });  
     }
   }
+  
   render() {
+    const isCompleted = this.props.completed 
+    const hover = this.state.hover
+    const showMenu = this.state.showMenu
+
     return (
-      <span>
-        <span onClick={this.props.removeTodo}>
-            X
+      <div className="todo-item" onMouseEnter={this.toggleHover.bind(this, true)} onMouseLeave={this.toggleHover.bind(this,false)}>
+        <span onClick={!showMenu ? this.props.toggleEditing : null} style={{textDecoration: isCompleted ? 'line-through' : 'none'}}>
+          {this.props.name}
         </span>
+        &nbsp; 
+        {this.props.due ? this.props.due.toLocaleDateString() : ''}
         &nbsp;
-        <i onClick={this.showMenu} class="far fa-calendar-plus"></i>
+        {/* {hover ? <HoverItems /> : ''} */}
         {
-          this.state.showMenu
+          hover 
             ? (
-              <div 
-                ref={(element) => {
-                  this.dropdownMenu = element;
-                }}
-              >
-                <button>item 1</button>
-                <button>item 2</button>
-                <button>item 3</button>
-              </div>
-          )
+              <span>
+                <span onClick={this.props.removeTodo}>
+                    X
+                </span>
+                &nbsp;
+                <i onClick={this.showMenu} className="far fa-calendar-plus"></i>
+                {
+                  showMenu
+                    ? (
+                      <div 
+                        ref={(element) => {
+                          this.dropdownMenu = element;
+                        }}
+                      >
+                        <Calendar />
+                      </div>
+                  )
+                    : (
+                      null
+                  )
+                } 
+              </span>
+            )
             : (
               null
-          )
+            )
         }
-        
-      </span>
+      </div>
     )
   }
-  
 }
+
+
+
 
 export default TodoItem
